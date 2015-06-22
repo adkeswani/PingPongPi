9f1f9ac27196bc4c14e52841a76673477762d59d
Adi <adkeswani@gmail.com>
2015-06-21 18:20:54 -0700

    Finished checkLastCapture tests

--- motion/test/testCheckLastCapture.py
+++ motion/test/testCheckLastCapture.py
@@ -2,9 +2,16 @@ import unittest
 from subprocess import check_output, CalledProcessError
 import os
 import shutil
+import time
 
 SCRIPT_CMD = "../checkLastCapture"
 CAPTURES_DIR = "testCaptures"
+
+def openFileAndGetModifiedTime(name):
+    testFilePath = os.path.join(CAPTURES_DIR, name)
+    testFile = open(testFilePath, "w")
+    modifiedTime = check_output(["date", "-r", testFilePath])
+    return testFilePath, modifiedTime
 
 class TestCheckLastCapture(unittest.TestCase):
 
@@ -35,7 +42,43 @@ class TestCheckLastCapture(unittest.TestCase):
 
     def test_no_files(self):
         output = check_output([SCRIPT_CMD, CAPTURES_DIR])
+
         self.assertEqual(output, "No captures found in " + CAPTURES_DIR + "\n");
+
+    def test_one_file(self):
+        path, modified = openFileAndGetModifiedTime("test")
+        output = check_output([SCRIPT_CMD, CAPTURES_DIR])
+
+        self.assertEqual(output, "Last motion was " + path + " captured at " + modified)
+
+        captures = os.listdir(CAPTURES_DIR)
+        self.assertEqual(len(captures), 1)
+        self.assertEqual(captures[0], "test");
+
+    def test_multiple_files(self):
+        path, modified = openFileAndGetModifiedTime("test")
+        time.sleep(1); # Otherwise the last modified times are too close together
+        path2, modified2 = openFileAndGetModifiedTime("test2")
+        output = check_output([SCRIPT_CMD, CAPTURES_DIR])
+
+        self.assertEqual(output, "Last motion was " + path2 + " captured at " + modified2)
+
+        captures = os.listdir(CAPTURES_DIR)
+        self.assertEqual(len(captures), 1)
+        self.assertEqual(captures[0], "test2");
+
+    def test_run_twice(self):
+        path, modified = openFileAndGetModifiedTime("test")
+        output = check_output([SCRIPT_CMD, CAPTURES_DIR])
+        time.sleep(1); # Otherwise the last modified times are too close together
+        path2, modified2 = openFileAndGetModifiedTime("test2")
+        output = check_output([SCRIPT_CMD, CAPTURES_DIR])
+
+        self.assertEqual(output, "Last motion was " + path2 + " captured at " + modified2)
+
+        captures = os.listdir(CAPTURES_DIR)
+        self.assertEqual(len(captures), 1)
+        self.assertEqual(captures[0], "test2");
 
 if __name__ == '__main__':
     unittest.main()
